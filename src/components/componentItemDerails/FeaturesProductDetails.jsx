44ffa3089ade94d09568307f42c8c8de5e44ce5b
adelramadan21 <adlramdn2468@outlook.com>
2025-12-15 22:06:02 +0200

    adel 2

---
+++ src/components/componentItemDerails/FeaturesProductDetails.jsx
@@ -0,0 +1,45 @@
+import car from "../../assets/delivery-truck-svgrepo-com (1) 1.png";
+import verify from "../../assets/verify.png";
+import shoop from "../../assets/shop-2-svgrepo-com 2.png";
+
+export default function FeaturesProductDetails() {
+  return (
+    <div className="w-full grid grid-cols-3 gap-4 px-2 py-5 items-center">
+      <div className="flex gap-4 items-center justify-center ">
+        <div className="w-14 h-14 bg-gray-200 rounded-2xl flex items-center justify-center">
+          <img className="w-7" src={car} alt="" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="whitespace-nowrap text-gray-700 text-[16px]">
+            Free Delivery
+          </p>
+          <p className="whitespace-nowrap text-[16px]">1-2 day</p>
+        </div>
+      </div>
+
+      <div className="flex gap-4 items-center justify-center ">
+        <div className="w-14 h-14 bg-gray-200 rounded-2xl flex items-center justify-center">
+          <img className="w-7" src={shoop} alt="" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="whitespace-nowrap text-gray-700 text-[16px]">
+            In Stock
+          </p>
+          <p className="whitespace-nowrap text-[16px]">Today</p>
+        </div>
+      </div>
+
+      <div className="flex gap-4 items-center justify-center ">
+        <div className="w-14 h-14 bg-gray-200 rounded-2xl flex items-center justify-center">
+          <img className="w-7" src={verify} alt="" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="whitespace-nowrap text-gray-700 text-[16px]">
+            Guaranteed
+          </p>
+          <p className="whitespace-nowrap text-[16px]">1 year</p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/componentItemDerails/RelatedProduct.jsx
@@ -0,0 +1,35 @@
+import phone1 from '../../assets/phone1.png';
+
+export default function RelatedProduct() {
+    return (
+        <div className="w-full py-5 flex flex-col gap-5 px-3">
+            <h3 className="text-2xl font-bold font-roboto">Related Products</h3>
+            <div className="w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                <div className='flex flex-col gap-5 px-6 py-7 bg-gray-200 rounded'>
+                    <img className='rounded' src={phone1} alt="" />
+                    <h4 className='font-bold'>Product Name</h4>
+                    <p className='font-semibold'>$199.99</p>
+                    <button className="btn text-white bg-dark w-full hover:bg-transparent hover:text-dark">Add To Cart</button>
+                </div>
+                <div className='flex flex-col gap-5 px-6 py-7 bg-gray-200 rounded'>
+                    <img className='rounded' src={phone1} alt="" />
+                    <h4 className='font-bold'>Product Name</h4>
+                    <p className='font-semibold'>$199.99</p>
+                    <button className="btn text-white bg-dark w-full hover:bg-transparent hover:text-dark">Add To Cart</button>
+                </div>
+                <div className='flex flex-col gap-5 px-6 py-7 bg-gray-200 rounded'>
+                    <img className='rounded' src={phone1} alt="" />
+                    <h4 className='font-bold'>Product Name</h4>
+                    <p className='font-semibold'>$199.99</p>
+                    <button className="btn text-white bg-dark w-full hover:bg-transparent hover:text-dark">Add To Cart</button>
+                </div>
+                <div className='flex flex-col gap-5 px-6 py-7 bg-gray-200 rounded'>
+                    <img className='rounded' src={phone1} alt="" />
+                    <h4 className='font-bold'>Product Name</h4>
+                    <p className='font-semibold'>$199.99</p>
+                    <button className="btn text-white bg-dark w-full hover:bg-transparent hover:text-dark">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
